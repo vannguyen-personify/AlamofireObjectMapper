21c27105311cabe0261da58da6949d6ffba593ce
van.nguyen <van.nguyen@personifyinc.com>
2022-02-14 15:19:31 +0700

    - Add all dependencies

--- Package.swift
+++ Package.swift
@@ -16,6 +16,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.5.0")),
+        .package(url: "https://github.com/tristanhimmelman/ObjectMapper.git", .upToNextMajor(from: "4.1.0"))
         
     ],
     targets: [
@@ -23,7 +24,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "AlamofireObjectMapper",
-            dependencies: []),
+            dependencies: ["Alamofire", "ObjectMapper"]),
         .testTarget(
             name: "AlamofireObjectMapperTests",
             dependencies: ["AlamofireObjectMapper"]),
